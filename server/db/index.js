1720850ab45e428166cbbd63e38f16b1fc494740
ez-li <eileenli.sf@gmail.com>
2019-08-07 21:41:00 -0700

    Established connection to sql server

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,23 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'student',
+  password: 'student',
+  database: 'chat'
+})
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected to mysql server');
+})
+
+// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+//   if (err) throw err
+
+//   console.log('The solution is: ', rows[0].solution)
+// })
+
+connection.end()
 
